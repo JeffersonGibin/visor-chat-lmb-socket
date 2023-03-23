2ee4c016753f13f16491a85015de2351c24f3521
Jefferson Gibin <jefferson@jefferson.gibin>
2023-03-23 13:14:58 -0300

    chat completation

--- src/repository/open-ai-gpt.repository.js
+++ src/repository/open-ai-gpt.repository.js
@@ -14,14 +14,9 @@ export class OpenAiGPTRepository {
     const res = await openai
       .createChatCompletion({
         model: "gpt-3.5-turbo",
-        prompt: message,
-        temperature: 0.5,
-        max_tokens: 3500,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
+        messages: [{ role: "user", content: message }],
       })
-      .then((res) => res.data.choices[0].text);
+      .then((res) => res.data.choices[0].message.content);
 
     return res;
   }
